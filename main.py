427d10b30b0a8736a740b43c9894a49e47da0031
tiko2401 <Fibi+260417!>
2021-02-04 11:18:05 +0100

    #002 - Start KalmanFilter

--- main.py
+++ main.py
@@ -4,3 +4,16 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+class KalmanFilter()
+    def __init__(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def update(self):
+        pass
+
+
+    def run(self, n_iterations):
+        pass
